75d8cdb9abc512d8133f8f02ac6b06f97d609e31
orzech <orzech@DESKTOP-BE8VQUO>
2021-03-20 00:56:06 +0100

    ikonki mapy / gna

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
 import '../styles/Map.css';
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 class Map extends React.Component {
   render() {
     return <div className={'map'}>
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[52.422058,16.973800]} zoom={30} scrollWheelZoom={false}>
         <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[52.422058,16.973800]}>
             <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
